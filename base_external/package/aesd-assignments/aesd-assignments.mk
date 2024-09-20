f1cb1d629ce19183e9a6beafa081cd35de6da11d
Juri Kuhn <kuhn@in.tum.de>
2024-09-20 23:07:38 +0000

    commit hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = dc438ecbe1ba6fd699618b953c42cb54947a583c
+AESD_ASSIGNMENTS_VERSION = 0aefab031ec8d8a3c5c188e1357850955176a3ef
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
